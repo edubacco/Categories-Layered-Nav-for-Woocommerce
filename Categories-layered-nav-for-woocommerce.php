1d4167cbfc26233ccba89af24d49129d2239e3fe
edu <eduardo3q@gmail.com>
2016-08-18 12:41:38 +0200

    added support for woocommerce 2.6.5 and for all taxonomies linked to products

--- Categories-layered-nav-for-woocommerce.php
+++ Categories-layered-nav-for-woocommerce.php
@@ -6,16 +6,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * Plugin Name: Categories Layered Navigation For Woocommerce
- * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Plugin URI: https://github.com/edubacco/Categories-Layered-Nav-for-Woocommerce
  * Description: This plug in adds a Widget that lets you add filters for taxonomies like Product Categories to the Layered Nav in Woocommerce. Requires Woocommerce.
  * Version: 1.0
- * Author: oscb
- * Author URI: http://oscarbazaldua.com
+ * Author: edubacco (thanks to oscb)
  * License: GPL2
- * WC requires at least: 2.2
- * WC tested up to: 2.3
+ * WC requires at least: 2.6.0
+ * WC tested up to: 2.6.4
  *
- * @author oscb (Oscar Bazaldua)
+ * @author edubacco (Eduardo Silvi)
  * @requires Woocommerce
  */
 
@@ -28,7 +27,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	/**
 	 * ob_add_categories_filter function
 	 *
-	 * Sets the global $chosen_attributes to include the Product Categories
+	 * Sets a global $chosen_cat_attributes to include the Product Categories
 	 *
 	 * @param array $filtered_posts
 	 *
@@ -67,16 +66,21 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	}
 
 	/**
-	 * ob_replace_layered_nav_widget
+	 * ob_layered_nav_widget
 	 *
-	 * Adds the Widget Class after the Woocommmerce plugins are loaded so it
+	 * Adds the Widgets Classes after the Woocommmerce plugins are loaded so it
 	 * can extend the Layered Nav Plugin
 	 *
 	 * @return void
 	 */
-	function ob_add_layered_nav_widget() {
+	function ob_layered_nav_widget() {
 		include_once( 'widgets/OB_Widget_Layered_Nav_Categories.php' );
 		register_widget( 'OB_Widget_Layered_Nav_Categories' );
+
+		include_once('widgets/OB_Widget_Layered_Nav_Filters.php');
+		unregister_widget('WC_Widget_Layered_Nav');
+		register_widget( 'OB_Widget_Layered_Nav_Filters' );
+
 	}
 
 	function add_tax_query_for_layered_cat_filters($tax_query, $wc_query) {
@@ -85,7 +89,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		if (!$_chosen_cat_attributes) ob_add_categories_filter([]);
 
 		// Layered nav filters on terms
-		if ( $_chosen_cat_attributes ) {
+		if (0 && $_chosen_cat_attributes ) {
 			foreach ( $_chosen_cat_attributes as $taxonomy => $data ) {
 				$tax_query[] = array(
 					'taxonomy' => $taxonomy,
@@ -100,14 +104,44 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		return $tax_query;
 	}
 
-	// Set Actions and Filters
-	add_action( 'widgets_init', 'ob_add_layered_nav_widget', 11 );
+	/**
+	 * loop_shop_post_in_layered_cat_filters
+	 *
+	 * filter products for chosen layered filter
+	 *
+	 * @param $ids array
+	 * @return array
+	 */
+	function loop_shop_post_in_layered_cat_filters($ids) {
+		global $_chosen_cat_attributes;
+		if ($_chosen_cat_attributes) {
+			$products_ids =  [];
+			$q['post_type'] = 'product';
+			foreach ($_chosen_cat_attributes as $taxonomy => $data) {
+				$q['tax_query'][] = [
+					'taxonomy' => $taxonomy,
+					'field' => 'slug',
+					'terms' => $data['terms'],
+					'operator' => 'and' === $data['query_type'] ? 'AND' : 'IN',
+					'include_children' => false,
+				];
+				$current_cat = get_queried_object();
+				if ($current_cat)
+					$q[$current_cat->taxonomy] = $current_cat->slug;
 
-	//add_filter( 'loop_shop_post_in', 'ob_add_categories_filter', 5, 1 );
+				$products = get_posts($q);
+				$products_ids = array_merge($products_ids, wp_list_pluck($products, 'ID'));
+			}
+			$ids = array_merge($products_ids, $ids);
+		}
+		return array_unique($ids);
+	}
+
+	// Set Actions and Filters
+	add_action( 'widgets_init', 'ob_layered_nav_widget', 11 );
+
+	add_filter( 'loop_shop_post_in', 'loop_shop_post_in_layered_cat_filters', 5, 1 );
 
 	add_filter( 'woocommerce_product_query_tax_query', 'add_tax_query_for_layered_cat_filters', 10, 2);
 
 }
-
-//todo: il widget che mostra i layered attivi non mostra il layered scelto attualmente. sistema questa cosa o trova una
-// soluzione alternativa a sti cazzo di layered filter
--- widgets/OB_Widget_Layered_Nav_Categories.php
+++ widgets/OB_Widget_Layered_Nav_Categories.php
@@ -122,8 +122,6 @@ class OB_Widget_Layered_Nav_Categories extends WC_Widget_Layered_Nav {
 
 			ob_start();
 
-			$found = false;
-
 			$this->widget_start( $args, $instance );
 
 			$found = $this->layered_nav_list($terms, $taxonomy, $query_type);
@@ -136,5 +134,83 @@ class OB_Widget_Layered_Nav_Categories extends WC_Widget_Layered_Nav {
 				echo ob_get_clean();
 			}
 		}
+	}
+
+
+	protected function layered_nav_list( $terms, $taxonomy, $query_type ) {
+		global $_chosen_cat_attributes;
+
+		// List display
+		echo '<ul>';
+
+		$term_counts        = $this->get_filtered_term_product_counts( wp_list_pluck( $terms, 'term_id' ), $taxonomy, $query_type );
+		//$_chosen_cat_attributes = WC_Query::get_layered_nav_chosen_attributes();
+		$found              = false;
+
+		foreach ( $terms as $term ) {
+			$current_values    = isset( $_chosen_cat_attributes[ $taxonomy ]['terms'] ) ? $_chosen_cat_attributes[ $taxonomy ]['terms'] : array();
+			$option_is_set     = in_array( $term->slug, $current_values );
+			$count             = isset( $term_counts[ $term->term_id ] ) ? $term_counts[ $term->term_id ] : 0;
+
+			// skip the term for the current archive
+			if ( $this->get_current_term_id() === $term->term_id ) {
+				continue;
+			}
+
+			// Only show options with count > 0
+			if ( 0 < $count ) {
+				$found = true;
+			} elseif ( 'and' === $query_type && 0 === $count && ! $option_is_set ) {
+				continue;
+			}
+
+			$filter_name    = 'filter_' . sanitize_title( str_replace( 'pa_', '', $taxonomy ) );
+			$current_filter = isset( $_GET[ $filter_name ] ) ? explode( ',', wc_clean( $_GET[ $filter_name ] ) ) : array();
+			$current_filter = array_map( 'sanitize_title', $current_filter );
+
+			if ( ! in_array( $term->slug, $current_filter ) ) {
+				$current_filter[] = $term->slug;
+			}
+
+			$link = $this->get_page_base_url( $taxonomy );
+
+			// Add current filters to URL.
+			foreach ( $current_filter as $key => $value ) {
+				// Exclude query arg for current term archive term
+				if ( $value === $this->get_current_term_slug() ) {
+					unset( $current_filter[ $key ] );
+				}
+
+				// Exclude self so filter can be unset on click.
+				if ( $option_is_set && $value === $term->slug ) {
+					unset( $current_filter[ $key ] );
+				}
+			}
+
+			if ( ! empty( $current_filter ) ) {
+				$link = add_query_arg( $filter_name, implode( ',', $current_filter ), $link );
+
+				// Add Query type Arg to URL
+				if ( $query_type === 'or' && ! ( 1 === sizeof( $current_filter ) && $option_is_set ) ) {
+					$link = add_query_arg( 'query_type_' . sanitize_title( str_replace( 'pa_', '', $taxonomy ) ), 'or', $link );
+				}
+			}
+
+			echo '<li class="wc-layered-nav-term ' . ( $option_is_set ? 'chosen' : '' ) . '">';
+
+			echo ( $count > 0 || $option_is_set ) ? '<a href="' . esc_url( apply_filters( 'woocommerce_layered_nav_link', $link ) ) . '">' : '<span>';
+
+			echo esc_html( $term->name );
+
+			echo ( $count > 0 || $option_is_set ) ? '</a> ' : '</span> ';
+
+			echo apply_filters( 'woocommerce_layered_nav_count', '<span class="count">(' . absint( $count ) . ')</span>', $count, $term );
+
+			echo '</li>';
+		}
+
+		echo '</ul>';
+
+		return $found;
 	}
 }
